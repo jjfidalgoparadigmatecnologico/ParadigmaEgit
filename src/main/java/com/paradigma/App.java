adb2bfbd4ddc794d97dd225a27e7dfa7d1d5546c
Juan Jose Fidalgo Moran <jjfidalgo@gmail.com>
2012-05-18 14:55:59 +0200

    aa

--- src/main/java/com/paradigma/App.java
+++ src/main/java/com/paradigma/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola Mundo desde newbranch" );
+        System.out.println( "Hola Mundo desde newbranch.dd" );
     }
 }
